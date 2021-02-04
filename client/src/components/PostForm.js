4d0826f4cc722b172dac53c160074e03d052e800
MohammadABH <mohammad.albinhassan@kcl.ac.uk>
2021-02-04 01:29:58 +0000

    Add create post functionality

---
+++ client/src/components/PostForm.js
@@ -0,0 +1,88 @@
+import React, { useContext } from "react";
+import { Button, Form } from "semantic-ui-react";
+import { useMutation } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+import { useForm } from "../util/hooks";
+import { FETCH_POSTS_QUERY } from "../util/graphql";
+
+function PostForm(props) {
+  const { onChange, onSubmit, values } = useForm(createPostCallback, {
+    body: "",
+  });
+
+  const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
+    variables: values,
+    update(proxy, result) {
+      const data = proxy.readQuery({
+        query: FETCH_POSTS_QUERY,
+      });
+      data.getPosts = [result.data.createPost, ...data.getPosts];
+	  proxy.writeQuery({
+		query: FETCH_POSTS_QUERY, data: {
+			getPosts: [result.data.createPost, ...data.getPosts]
+		}
+	})
+      values.body = "";
+	},
+	onError(err){
+		// Error handled below, this is just to prevent throwing errors
+	}
+  });
+
+  function createPostCallback() {
+    createPost();
+  }
+  return (
+    <>
+      <Form onSubmit={onSubmit}>
+        <h2>Create a Post:</h2>
+        <Form.Field>
+          <Form.Input
+            placeholder="Hi World"
+            name="body"
+            onChange={onChange}
+            value={values.body}
+            error={error ? true : false}
+          />
+          <Button type="submit" color="teal">
+            Submit
+          </Button>
+        </Form.Field>
+      </Form>
+      {error && (
+        <div className="ui error message" style={{ marginBottom: 20 }}>
+          <ul className="list">
+            <li>{error.graphQLErrors[0].message}</li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+}
+
+const CREATE_POST_MUTATION = gql`
+  mutation createPost($body: String!) {
+    createPost(body: $body) {
+      id
+      username
+      body
+      createdAt
+      likeCount
+      likes {
+        id
+        username
+        createdAt
+      }
+      commentCount
+      comments {
+        id
+        body
+        username
+        createdAt
+      }
+    }
+  }
+`;
+
+export default PostForm;
